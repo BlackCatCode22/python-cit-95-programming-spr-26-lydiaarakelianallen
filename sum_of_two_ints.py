12f0b6ca35fa7a68314276d98d547ec486966283
Lydia Allen <lydia.arakelian.allen@gmail.com>
2026-01-20 10:28:26 -0800

    adds two integers provided by the user

---
+++ sum_of_two_ints.py
@@ -0,0 +1,8 @@
+# gets the first integer from the user and converts it from a string to an integer
+first = input("Enter first number: ")
+first = int(first)
+# gets the second integer from the user and convers from a string to an integer
+second = input("Enter second number: ")
+second = int(second)
+# adds the two integers
+print(first + second)
