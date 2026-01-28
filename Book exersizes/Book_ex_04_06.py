26294e89a536e83777159a64f730f144251bc5bb
Lydia Allen <lydia.arakelian.allen@gmail.com>
2026-01-28 15:26:22 -0800

    exersize 4.6 Function

--- Book exersizes/Book_ex_04_06.py
+++ Book exersizes/Book_ex_04_06.py
@@ -1,12 +1,12 @@
 def computepay(hours, rate):
-    print("In computepay", hours, rate)
+    # print("In computepay", hours, rate)
     if hours > 40:
         reg = rate * hours
         otp = (hours - 40.0) * (rate * 0.5)
         pay = reg + otp
     else:
         pay = hours * rate
-    print("Returning", pay)
+    # print("Returning", pay)
     return pay
 
 sh = input("Enter Hours: ")
@@ -17,4 +17,4 @@ fr = float(sr)
 
 xp = computepay(fh,fr)
 
-print("Pay: ",xp)
+print("Pay",xp)
